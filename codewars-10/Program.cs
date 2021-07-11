b3133dd59d5eec2535add492cef79e2ac62e503a
HornyFerret <ombro.de.morto@gmail.com>
2021-07-11 21:04:11 +0300

    Добавьте файлы проекта.

---
+++ codewars-10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace codewars_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lst = new int[] { 1, 5, 6, 2, 3 };
+            for (int i = 0; i < lst.Length; i++)
+            {
+                Console.WriteLine(lst[i]);
+            }
+            Console.WriteLine("------");
+            int[] w = minMax(lst);
+            for (int i = 0; i < w.Length; i++)
+            {
+                Console.WriteLine(w[i]);
+            }
+        }
+        public static int[] minMax(int[] lst)
+        {
+            int n = lst.Min();
+            int m = lst.Max();
+            int[] f = new int [] { n, m };
+            return f;
+        }
+    }
+}
